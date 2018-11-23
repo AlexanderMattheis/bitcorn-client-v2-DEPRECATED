f3779dc568a6228080d5d5733fa01aa8f9b3f475
Alex <almat@gmx.de>
2018-11-23 09:10:50 +0100

    Fixed remaining problem.

--- tests/unit/transforms/array-test.ts
+++ tests/unit/transforms/array-test.ts
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
+// @ts-ignore
 module('transform:array', 'Unit | Transform | array', function(hooks) {
   setupTest(hooks);
 
