6194dcf1572fc59e88047266820a54d32abb8db4
Alex <almat@gmx.de>
2018-12-09 16:16:35 +0100

    Added model for communication with backend.

--- app/routes/contact.ts
+++ app/routes/contact.ts
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class Contact extends Route.extend({
+  model(): object {
+    return {
+      name: "",
+      email: "",
+      message: ""
+    };
+  }
 }) {
   // normal class body definition here
 }
